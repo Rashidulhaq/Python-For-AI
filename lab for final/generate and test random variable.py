3c3e7f6910c95f5b81b84c392608bf0013df3786
Rashidulhaq <77320958+Rashidulhaq@users.noreply.github.com>
2023-02-20 13:26:32 +0600

    Create generate and test random variable.py

---
+++ lab for final/generate and test random variable.py
@@ -0,0 +1,22 @@
+import random
+
+
+def fx(x):
+    return (x * x) - (5 * x) + 6
+
+
+result = None
+count = 0
+
+while result != 0:
+    # Generate a random number between -10 and 10
+    num = random.randint(-10, 10)
+    print("Generated number:", num)
+
+    # Calculate the result of f(x)
+    result = fx(num)
+    print("f(x) result:", result)
+
+    count += 1
+
+print("Zero was generated after", count, "iterations.")
